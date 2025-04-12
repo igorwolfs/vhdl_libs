23edee80c0ef73432911978c64b07b0bd392ffa4
igorwolfs <wolfs.igor@hotmail.com>
2025-04-12 19:52:56 +0200

    [i2c] minor bugfix, created testbench, [next] Check issues with ADDR to ACK transition

--- modules/verilog/i2c/i2c_slave.v
+++ modules/verilog/i2c/i2c_slave.v
@@ -1,3 +1,4 @@
+`timescale 1ns / 1ps
 
 
 module i2c_slave (
@@ -12,13 +13,13 @@ module i2c_slave (
 	input 				NRST,
 	input wire 			ACKA_RDY,		//> Indicates whether a stalling of the bus should happen on ACKA (because data fetching isn't done yet)
 	input wire 			ACKD_RDY,		//> Indicates whether a stalling of bus should happen on ACKD (because data writing isn't done yet)
-	output wire 		BUSY
-)
+	output reg  		BUSY
+);
 
-localparam S_IDLE = 0,
-localparam S_ADDR = 1,
+localparam S_IDLE = 0;
+localparam S_ADDR = 1;
 localparam S_ACKA = 2;
-localparam S_DATA = 3,
+localparam S_DATA = 3;
 localparam S_ACKD = 4;
 localparam S_STOP = 5;
 
@@ -28,11 +29,11 @@ localparam RW_READ = 1'b1;
 reg [2:0] counter;
 reg [7:0] data_internal;
 reg r_w_internal;
-reg [2:0] s_next;
-reg [2:0] s_curr;
-reg scl_slave, sda_slave, scl_drive, sda_drive;
+reg [2:0] next_state;
+reg [2:0] curr_state;
+reg sda_slave, scl_drive, sda_drive;
 
-assign SCL = (scl_drive) ? scl_slave : 1'bz;
+assign SCL = (scl_drive) ? 1'b0 : 1'bz;
 assign SDA = (sda_drive) ? sda_slave : 1'bz;
 
 
@@ -45,28 +46,26 @@ assign SDA = (sda_drive) ? sda_slave : 1'bz;
 always @(negedge SCL, negedge NRST)
 begin
 	if (~NRST)
-		s_curr <= S_IDLE;
+		curr_state <= S_IDLE;
 	else
 	begin
-		s_curr <= s_next;
-		case (s_next)
+		curr_state <= next_state;
+		case (next_state)
 			S_ACKA:
 			begin
-				scl_slave <= 0;
 				sda_slave <= 0;
 			end
 			S_DATA: //> Data write should happen on negedge
 			begin
 				if (RW == RW_READ)
 				begin
-					sda_slave <= IDATA[counter];
 					counter <= counter + 1;
+					sda_slave <= IDATA[counter];
 				end
 				else;
 			end
 			S_ACKD:
 			begin
-				scl_slave <= 0;
 				sda_slave <= 0;
 			end
 		endcase
@@ -79,13 +78,13 @@ end
 
 
 always @(posedge SCL or negedge NRST) begin
-	if (~NRST) 
+	if (~NRST)
 	begin
 	  counter <= 0;
 	end
 	else 
 	begin
-	  case (s_curr)
+	  case (curr_state)
 		S_ADDR:
 		begin
 			if (counter < 7)
@@ -138,7 +137,7 @@ begin
 		else;
 	end
 	else
-		start_condition <= 0; //> Set start condition to 0 when SCL goes low (since s_curr should then be set to s_next)
+		start_condition <= 0; //> Set start condition to 0 when SCL goes low (since curr_state should then be set to next_state)
 end
 
 //> STOP CONDITION DETECTOR
@@ -159,21 +158,21 @@ begin
 end
 
 always @(*) begin
-	s_next = S_IDLE;  // default “no change”
+	next_state = S_IDLE;  // default “no change”
 	scl_drive = 1'b0;
 	sda_drive = 1'b0;
 	ARDY = 1'b0;
 	DRDY = 1'b0;
 	BUSY = 1'b1;
 
-	case (s_curr)
+	case (curr_state)
 		S_IDLE:
 		begin
 			if (start_condition)
-				s_next = S_ADDR;
+				next_state = S_ADDR;
 			else
 			begin
-				s_next = S_IDLE;
+				next_state = S_IDLE;
 				BUSY = 1'b1;
 			end
 		end
@@ -181,7 +180,9 @@ always @(*) begin
 		S_ADDR: //> Wait until address + R/W is received
 		begin
 			if (counter > 7)
-				s_next = S_ACKA;
+				next_state = S_ACKA;
+			else 
+				next_state = S_ADDR;
 		end
 		
 		S_ACKA:	//> Address-ready out, waiting for data-in ready
@@ -190,22 +191,28 @@ always @(*) begin
 			if (ACKA_RDY) 			//> Should be enabled until the ARDY out turns low
 			begin
 				sda_drive = 1'b1; 	//> Drive SDA low
-				s_next = S_DATA;
+				next_state = S_DATA;
 			end
 			else
+			begin
 				scl_drive = 1'b1; //> Stall
+				next_state = S_ACKA;
+			end
 		end
 		
 		S_DATA: 
 		begin
 			if (counter > 7)
 			begin
-				S_next = S_ACKD;
+				next_state = S_ACKD;
 			end
 			else
 			begin
 				if (RW == RW_READ)
+				begin
+					next_state = S_DATA;
 					sda_drive = 1'b1; //> Slave drive SDA when data write
+				end
 			end
 		end
 		
@@ -215,89 +222,26 @@ always @(*) begin
 			if (ACKD_RDY) 		  //> Should be enabled until DRDY out turns low
 			begin
 				sda_drive = 1'b1; //> Drive SDA low
-				s_next = S_STOP;
+				next_state = S_STOP;
 			end
 			else
+			begin
 				scl_drive = 1'b1; //> Stall
+				next_state = S_ACKD;
+			end
 		end
 
 		S_STOP: 
 		begin
 			if (stop_condition)
-				s_next = S_IDLE;
-			else;
+				next_state = S_IDLE;
+			else
+				next_state = S_STOP;
 		end
 		
-		default: s_next = IDLE;
+		default: next_state = S_IDLE;
 	endcase
   end
 
 
 endmodule
-/**
-Questions
-- How does the controller on top of the i2c slave know when the address has received so it can start fetching from that address in case of a data read?
-	- Answer: use clock stretching, pull the clock low until the data is fetched.
-		- The master must normally detect this clock stretching (?how).
-		- If the master has detected it it stalls.
-
-
-- IDLE: 
-	- @negedge SDA AND SCL high -> next_state = ADDR
-		- @negedge SCL: IDLE -> ADDR
-- ADDR:
-	- @posedge SCL: 
-		- if addr_index < 6, adddr_index ++
-		- if addr_index == 7, set R_W
-	- @negedge SCL: 
-		- if addr_index > 7
-			- ADDR -> STALLA
-			- SCL low
-- STALLA:
-	- @posedge ACKA_RDY: STALLA -> ACKA
-		- SCL loose
-		- SDA low
-- ACKA:
-	- @negedge SCL: ACKA -> DATA
-- DATA:
-	- @posedge SCL:
-		- if data index <= 7:
-			- Store in data
-	- @negedge SCL:
-		- if data index <= 7:
-			- next state is DATA
-		- if data_index == 8:
-			- next state is STALLD
-			- SCL should be pulled low
-- STALLD:
-	- @posedge ACKD_RDY: STALLD -> ACKD
-		- Pull SDA low
-- ACKA:
-	- @negedge SCL: ACKA -> STOP
-- STOP:
-	- @posedge SDA: STOP -> IDLE
-
-NOTE:
-- @negedge SCL: State transitions: S_curr <= S_next
-- @posedge SCL: Sampling, data operations and next_state set based on curr_state
-- in case of stalling -> just disable any state-related logic
-	-> pull the SCL low UNTIL the correct drdy is triggered
-
-
-
-We can drive the scl signals either
-- In the negedge-loop and add 2 signals
-- In the combinatorial loop
-
-It seems like the best way to solve the issues here is to
-- Have the ACK state and STALL together
-- By default the STALL is always enabled
-- It is ONLY unset once the relevant RDY-signal is asserted
-	- Once that RDY signal is asserted the ACK should in fact 
-		- Pull the SDA low
-		- Release the SCL
-	- HOWEVER: the SCL and SDA are then being set inside the comb-block
-		- Solution: set only the scl_drive and sda_drive inside the comb-block
-		- State transition then simply happens on the negedge
-		
-*/
